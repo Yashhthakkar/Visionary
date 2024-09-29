61f6a29b3f95493571e4fd295f22d47650a6fb44
Yash Thakkar <52297366+Yashhthakkar@users.noreply.github.com>
2024-09-29 07:45:19 -0400

    Add files via upload

---
+++ Visionary/VisionaryTests/VisionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionaryTests.swift
+//  VisionaryTests
+//
+//  Created by Yash Thakkar on 9/21/24.
+//
+
+import Testing
+@testable import Visionary
+
+struct VisionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
